def857a725339d87b0a9dc97fe50d58d6bf339e5
Elyes159 <elyesmlik307@gmail.com>
2024-04-16 22:51:48 +0100

    Your commit message here

--- khalil_wordpress/settings.py
+++ khalil_wordpress/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_!l8fowccay--stp@en(s&g1trng^&_4@-1gt1rnwb!^%hps+5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
